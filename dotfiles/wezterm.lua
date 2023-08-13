55e42fbdb6b257a8f5e1f1638e0f3be2996ccb36
Jenny Jingjing Li <jennyli12320@gmail.com>
2023-08-13 21:56:50 +1000

    fix wezterm on mac (hopefully this then doesn't break it on wsl again)

--- dotfiles/wezterm.lua
+++ dotfiles/wezterm.lua
@@ -8,29 +8,17 @@ local config = {}
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
-config.font = wezterm.font("Mononoki Nerd Font")
+config.font = wezterm.font("Mononoki Nerd Font", { weight = "Bold" })
 config.color_scheme = "rose-pine"
 config.font_size = 14
 
-config.use_resize_increments = true
+config.window_background_opacity = 0.95
 
-config.ssh_domains = {
-	{
-		name = "raspi",
-		remote_address = "192.168.1.104",
-		username = "jonkjonks",
-		ssh_option = { identityfile = "~/.ssh/id_rsa.pub" },
-	},
-}
-
-if not os.getenv("DISPLAY") then
+if not os.getenv("HOME") then
 	config.default_domain = "WSL:Arch"
 	config.default_prog = { "nu" }
 	config.default_cwd = "~"
-
-	config.window_background_opacity = 0.95
 else
-	config.window_background_opacity = 0.9
 	config.hide_tab_bar_if_only_one_tab = true
 	config.macos_window_background_blur = 20
 end
