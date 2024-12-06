0daf58b30171a89cb5e9b196b2507182ddc0b5ec
Jenny Li <jen@Jennys-Josef-Laptop.local>
2024-12-06 20:52:10 +1100

    aaa
    Signed-off-by: Jenny Li <jen@Jennys-Josef-Laptop.local>

---
+++ init.sh
@@ -0,0 +1,26 @@
+echo 'Install Homebrew'
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'Installing packages'
+curl -LsSf https://astral.sh/uv/install.sh | sh
+brew install fnm
+brew install neovim
+brew install stow
+
+echo 'Installing shell'
+brew install wezterm starship
+brew install zsh zsh-syntax-highlighting zsh-autosuggestions
+brew install macchina ccat lsd fzf ripgrep ast-grep
+brew install unzip wget curl gzip
+
+brew install lazygit
+
+echo 'Installing UI setup'
+brew install font-zedmono-nerd-font
+brew install felixkratz/formulae/borders
+brew install felixkratz/formulae/sketchybar
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
+
+echo 'Install programs'
+brew install 1password raycast spotify suspicious-package mullvadvpn betterdisplay karabiner-elements keka apparency appcleaner arc 1password-cli vscodium
--- shell/dot-wezterm.lua
+++ shell/dot-wezterm.lua
@@ -5,7 +5,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font("ZedMono Nerd Font", { weight = "SemiBold", stretch = "Expanded" })
+config.font = wezterm.font("ZedMono Nerd Font", { weight = "Medium", stretch = "Expanded" })
+config.font_size = 18
 -- For example, changing the color scheme:
 config.color_scheme = "Everblush"
 
