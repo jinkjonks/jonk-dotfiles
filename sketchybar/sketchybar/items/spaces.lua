8c4c0eb233c5f29d4cbfd306c0c1c0d573e9de8a
Jenny Li <jen@Jennys-Josef-Laptop.local>
2024-12-06 19:40:19 +1100

    sketchybar

---
+++ sketchybar/sketchybar/items/spaces.lua
@@ -0,0 +1,99 @@
+local colors = require("colors")
+local settings = require("settings")
+
+local spaces = {}
+
+for i = 1, 9, 1 do
+  local space = Sbar.add("space", "space." .. i, {
+    space = i,
+    icon = {
+      drawing = false,
+    },
+    label = {
+      string = string.format("%02d", i),
+      padding_left = settings.item_padding,
+      padding_right = settings.item_padding,
+      color = colors.grey,
+      highlight_color = colors.white,
+      align = "center",
+    },
+    background = {
+      height = settings.item_height,
+      color = colors.transparent, -- Start transparent
+      corner_radius = settings.item_corner_radius,
+    },
+    popup = {
+      background = {
+        border_width = settings.popup_border_width,
+        border_color = colors.red,
+        drawing = true -- Make sure popup can draw
+      }
+    }
+  })
+
+  spaces[i] = space
+
+  -- Padding space
+  Sbar.add("space", "space.padding." .. i, {
+    space = i,
+    script = "",
+    width = settings.spacings,
+  })
+
+  local space_popup = Sbar.add("item", {
+    position = "popup." .. space.name,
+    background = {
+      padding_left = settings.popup_border_width,
+      drawing = true,
+      image = {
+        corner_radius = settings.bar_corner_radius,
+        scale = 0.15
+      }
+    }
+  })
+
+  space:subscribe("space_change", function(env)
+    local selected = env.SELECTED == "true"
+    space:set({
+      icon = { highlight = selected },
+      label = {
+        highlight = selected,
+        string = string.format("%02d", tonumber(env.SID))
+      },
+      background = {
+        color = selected and colors.spaces.active or colors.spaces.inactive,
+      },
+      width = selected and 30 or 30
+    })
+  end)
+
+  space:subscribe("mouse.clicked", function(env)
+    if env.BUTTON == "other" then
+      space_popup:set({ background = { image = "space." .. env.SID } })
+      space:set({ popup = { drawing = "toggle" } })
+    else
+      local op = (env.BUTTON == "right") and "--destroy" or "--focus"
+      Sbar.exec("yabai -m space " .. op .. " " .. env.SID)
+    end
+  end)
+
+  space:subscribe("mouse.entered", function(env)
+    space_popup:set({ background = { image = "space." .. env.SID } })
+    space:set({ popup = { drawing = "toggle" } })
+  end)
+
+  space:subscribe("mouse.exited", function(_)
+    space:set({ popup = { drawing = false } })
+  end)
+end
+
+local space_window_observer = Sbar.add("item", {
+  drawing = false,
+  updates = true,
+})
+
+-- Add a spacer after the spaces
+Sbar.add("item", "spaces.spacer", {
+  width = settings.item_spacing,
+  background = { drawing = false }
+})
--- sketchybar/sketchybar/settings.lua
+++ sketchybar/sketchybar/settings.lua
@@ -1,1 +1,46 @@
-../../dotfiles/sketchybar/.config/sketchybar/settings.lua
+return {
+  bar_height = 28,
+  bar_corner_radius = 8,
+  bar_padding = 2,
+  bar_margin = 8,
+  bar_blur_radius = 8,
+
+  item_height = 24,
+  item_padding = 8,
+  item_corner_radius = 6,
+  item_spacing = 8,
+
+  group_spacing = 16,
+
+  popup_border_width = 2,
+  popup_blur_radius = 8,
+  popup_y_offset = 4,
+  popup_padding = 16,
+  popup_image_padding = 0,
+
+  -- Defaults settings
+  display = 1,
+  paddings = 0,
+
+  icons = "nerd font",    -- Options: "sf-symbols", "nerdfont"
+  animated_icons = false, -- Set to true if you want to use animated icons
+
+  font = {
+    text = "ZedMono Nerd Font",        -- Used for text
+    numbers = "ZedMono Nerd Font",     -- Used for numbers
+    icons = "ZedMono Nerd Font",       -- Used for icons (or NerdFont)
+    app_icons = "sketchybar-app-font", -- Used for app icons
+    sizes = {
+      text = 14.0,
+      numbers = 14.0,
+      icons = 14.0,
+      app_icons = 14.0,
+    },
+    style_map = {
+      ["Regular"] = "Regular",
+      ["Medium"] = "SemiBold",
+      ["Bold"] = "Bold",
+      ["Black"] = "ExtraBold"
+    }
+  }
+}
