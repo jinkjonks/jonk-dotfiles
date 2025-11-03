ac4f8c34b504d7ecfe4b1faffbc5026eaedaf7eb
Jenny Jingjing Li <jenny@joseflegal.com>
2025-11-03 17:15:28 +1100

    update python nvim config
    Signed-off-by: Jenny Jingjing Li <jenny@joseflegal.com>

--- neovim/dot-config/nvim/lua/config/options.lua
+++ neovim/dot-config/nvim/lua/config/options.lua
@@ -6,3 +6,23 @@
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.python3_host_prog = "~/nvim/bin/python3"
 vim.lsp.set_log_level("off")
+vim.g.augment_disable_tab_mapping = true
+
+vim.lsp.config("basedpyright", {
+  settings = {
+    basedpyright = {
+      disableOrganizeImports = true,
+      analysis = {
+        autoImportCompletions = true,
+        useTypingExtensions = true,
+        autoFormatStrings = true,
+        inlayHints = {
+          variableTypes = false,
+          callArgumentNames = false,
+        },
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+})
--- neovim/dot-config/nvim/lua/plugins/lang/python.lua
+++ neovim/dot-config/nvim/lua/plugins/lang/python.lua
@@ -1,36 +1,13 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-
+    "conform.nvim",
     opts = function(_, opts)
-      opts.servers.basedpyright = {
-        settings = {
-          basedpyright = {
-            analysis = {
-              autoImportCompletions = true,
-              useTypingExtensions = true,
-              autoFormatStrings = true,
-              inlayHints = {
-                variableTypes = false,
-                callArgumentNames = false,
-              },
-            },
-            autoSearchPaths = true,
-            useLibraryCodeForTypes = true,
-          },
-        },
+      opts.formatters_by_ft["python"] = {
+        "ruff_fix",
+        "ruff_format",
+        "ruff_organize_imports",
       }
-    end,
-  },
-  {
-    "roobert/f-string-toggle.nvim",
-    ft = "python",
-    config = function()
-      require("f-string-toggle").setup({
-        key_binding = "<leader>cpf",
-        key_binding_desc = "Toggle f-string",
-        filetypes = { "python" },
-      })
+      return opts
     end,
   },
 }
